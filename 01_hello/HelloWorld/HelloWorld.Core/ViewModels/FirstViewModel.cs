f274b1eb5a9d1c12eca13f0d686f4510d72372e6
Mazur.Michal YMH <ymh@kmd.dk>
2017-03-17 12:45:52 +0100

    Initial push for hello world solution

---
+++ 01_hello/HelloWorld/HelloWorld.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.Core.ViewModels;
+
+namespace HelloWorld.Core.ViewModels
+{
+    public class FirstViewModel
+        : MvxViewModel
+    {
+        private string _hello = "Hello MvvmCross";
+        public string Hello {
+            get { return _hello; }
+            set { SetProperty (ref _hello, value); }
+        }
+    }
+}
---
+++ 01_hello/HelloWorld/HelloWorld.Droid/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using MvvmCross.Droid.Views;
+
+namespace HelloWorld.Droid
+{
+    [Activity (
+        Label = "HelloWorld.Droid"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen ()
+            : base (Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
