681583087bded4a2abafa84b0f4e96276de48200
fgikmd <z8xfg@Filip.local>
2017-03-29 18:02:37 +0200

    Step 1: Clean up

--- 01_hello/HelloWorld/HelloWorld.Core/ViewModels/FirstViewModel.cs
+++ 01_hello/HelloWorld/HelloWorld.Core/ViewModels/FirstViewModel.cs
@@ -2,13 +2,7 @@
 
 namespace HelloWorld.Core.ViewModels
 {
-    public class FirstViewModel
-        : MvxViewModel
+    public class FirstViewModel : MvxViewModel
     {
-        private string _hello = "Hello MvvmCross";
-        public string Hello {
-            get { return _hello; }
-            set { SetProperty (ref _hello, value); }
-        }
     }
 }
