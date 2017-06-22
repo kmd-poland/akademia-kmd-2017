0448ba784c2d2b0e13b82468dea61202f5ef9f6e
Czechowski.Maciej <mcz@avaleo.net>
2017-06-22 21:46:24 +0200

    Create ImporterLekow project

---
+++ ImporterLekow/ImporterLekow/ProduktLeczniczyOpakownie.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Schemas;
+using SQLite;
+
+namespace ImporterLekow
+{
+    public class ProduktLeczniczyOpakowanie
+    {
+     
+        public string SubstancjeCzynne { get; set; }
+        public string Substancje { get; set; }
+
+
+        public string NazwaProduktu { get; set; }
+
+    
+        public string RodzajPreparatu { get; set; }
+
+        public string NazwaPowszechnieStosowana { get; set; }
+
+        public string Moc { get; set; }
+
+        public string Postac { get; set; }
+
+        public string PodmiotOdpowiedzielny { get; set; }
+
+	
+        public string NumerPozwolenia { get; set; }
+
+        public string WaznoscPozwolenia { get; set; }
+
+        public string KodATC { get; set; }
+
+        public string GatunkiDocelowe { get; set; }
+
+	
+
+        public string OkresyKarencji { get; set; }
+
+        public string Status { get; set; }
+
+
+		//zawartosc encji Opakowanie
+
+
+        public string Wielkosc { get; set; }
+
+        public string JednostkaWielkosci { get; set; }
+
+        //EAN jest naszym kluczem glownym!
+        [PrimaryKey]
+        public string KodEAN { get; set; }
+
+        public opakowanieMTKategoriaDostepnosci? KategoriaDostepnosci { get; set; }
+
+		
+        public ProduktLeczniczyOpakowanie()
+        {
+        }
+
+        // te dwie metody pozwalaja na porownanie obiektow na podstawie EAN (potrzebe do Distinct)
+		public override bool Equals(object obj)
+		{
+            return ((ProduktLeczniczyOpakowanie)obj).KodEAN == KodEAN;
+		}
+		public override int GetHashCode()
+		{
+            return KodEAN.GetHashCode();
+		}
+    }
+}
---
+++ ImporterLekow/ImporterLekow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Schemas;
+using System.Linq;
+using SQLite;
+
+namespace ImporterLekow
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            XmlSerializer serializer = new XmlSerializer(typeof(produktyLecznicze));
+
+            StreamReader reader = new StreamReader("RPL.xml");
+            var produktyLecznicze = (produktyLecznicze)serializer.Deserialize(reader);
+            reader.Close();
+
+
+            Console.WriteLine($"Finished reading {produktyLecznicze.produktLeczniczy.Length} records!");
+
+            // denormalize records
+            var denormalizedSet =
+                produktyLecznicze
+                    .produktLeczniczy
+                    .Select(p =>
+                          p.opakowania.Select(opakowanie =>
+                                    new ProduktLeczniczyOpakowanie
+                                    {
+                                        KodEAN = opakowanie.kodEAN,
+                                        NazwaProduktu = p.nazwaProduktu //i tak dalej
+                                    })
+                           ).SelectMany(x => x)
+                    .Where(x=>!string.IsNullOrEmpty(x.KodEAN)) //interesuja nas tylko opakowania z EAN
+                    .Distinct() //wystepuje jeden EAN, dla ktorego sa dwa produkty. Pewnie to jakis blad, ale musimy odfiltrowac - wybieramy losowy...
+                    .ToList()
+                    ;
+
+            //save 'em all
+            var connection = new SQLiteConnection("leki.db");
+            connection.DropTable<ProduktLeczniczyOpakowanie>();
+            connection.CreateTable<ProduktLeczniczyOpakowanie>();
+            connection.InsertAll(denormalizedSet);
+            connection.Close();
+
+        }
+    }
+}
